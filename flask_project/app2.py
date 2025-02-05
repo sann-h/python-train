07226459ff5aba2d54a944f6b0867c1dad9eef8f
sann-h <sandhiya22td0713@svcet.ac.in>
2025-02-05 12:44:52 +0530

    task2

---
+++ flask_project/app2.py
@@ -0,0 +1,20 @@
+from flask import Flask, request,jsonify
+
+app = Flask(__name__)
+
+menu = [
+    {"name": "chicken biriyani", "price": 120},
+    {"name": "mutton biriyani", "price":130},
+    {"name": "chicken 65", "price":100}
+]
+
+@app.route('/data',methods=['GET','POST'])
+def handle_data():
+    if request.method =='POST':
+        data = request.get_json()
+        menu.append(data)
+        return jsonify(data)
+    return jsonify(menu)
+
+if __name__ == '__main__':
+    app.run(debug=True)
